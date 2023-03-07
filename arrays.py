66a190a4d46c200951280c340b889f972e0f3dcc
Vancha <tjipkevanderheide90@gmail.com>
2023-03-07 16:19:47 +0100

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -7,59 +7,39 @@ The array data structure. This file will hold a couple things related to working
 - array search, insert and delete in sorted array
 - array sorting
 - generation of all subarrays
+After which the goal is to use that in solving a couple of leetcode questions
 
-After which the goal is to use that in solving a couple of the following questions:
-## Easy
-        Find the largest three elements in an array
-        Find Second largest element in an array
-        Move all zeroes to end of array
-        Rearrange array such that even positioned are greater than odd
-        Rearrange an array in maximum minimum form using Two Pointer Technique
-        Segregate even and odd numbers
-        Reversal algorithm for array rotation
-        Print left rotation of array in O(n) time and O(1) space
-        Sort an array in wave form
-        Sort an array which contain 1 to n values
-        Count the number of possible triangles
-        Print All Distinct Elements of a given integer array
-        Find the element that appears once in Array where every other element appears twice
-        Leaders in an array
-        Find sub-array with given sum
-## Medium
-        Rearrange an array such that arr[i] = i
-        Rearrange positive and negative numbers in O(n) time and O(1) extra space
-        Reorder an array according to given indexes
-        Search an element in a sorted and rotated array
-        Find the Rotation Count in Rotated Sorted array
-        K-th Largest Sum Contiguous Subarray
-        Find the smallest missing number
-        Difference Array | Range update query in O(1)
-        Maximum profit by buying and selling a share at most twice
-        Smallest subarray with sum greater than a given value
-        Inversion count in Array using Merge Sort
-        Sort an array of 0s, 1s and 2s
-        Merge two sorted arrays with O(1) extra space
-        Majority Element
-        Two Pointers Technique
-        Find a peak element
-        Find a triplet that sum to a given value
-        Minimum increment by k operations to make all elements equal
-        Equilibrium index of an array
-##  Hard
-        Find k numbers with most occurrences in the given array
-        MO’s Algorithm
-        Square Root (Sqrt) Decomposition Algorithm
-        Sparse Table
-        Range sum query using Sparse Table
-        Range Minimum Query (Square Root Decomposition and Sparse Table)
-        Range LCM Queries
-        Merge Sort Tree for Range Order Statistics
-        Minimum number of jumps to reach end
-        Space optimization using bit manipulations
-        Sort a nearly sorted (or K sorted) array
-        Find maximum value of Sum( i*arr[i]) with only rotations on given array allowed
-        Median in a stream of integers (running integers)
-        Construct an array from its pair-sum array
-        Maximum equlibrium sum in an array
-        Smallest Difference Triplet from Three arrays
-        Find all triplets with zero sum'''
+#Rotating an array left
+
+## Rotate left using a temporary array
+
+This approach rotates an array d positions to the left, using the following steps:
+    
+    - Store index d to the last index in a temporary array
+    - append the first index up to d of the original array to the temp array
+
+#Time: O(N)
+#Space: O(N)'''
+def rotate_array_left(arr, d):
+    temp_array = arr[d:len(arr)-1]
+    temp_array += arr[0:d]
+    return temp_array
+
+'''
+## Rotate left one by one
+
+This approach rotates an array left 1 position, using the following steps:
+    - store the first element in a temp variable
+    - shift all indexes to the left except for the last index
+    - set the last index equal to the temp variable
+
+#Time: O(N*d)
+#Space: O(1)
+'''
+def rotate_by_one(arr):
+    first = arr[0]
+    for (idx,y) in enumerate(arr):
+        if idx < len(arr)-1:
+            arr[idx] = arr[idx+1]
+    arr[len(arr)-1] = first
+    return arr
