c05abecab14bb205c6713d050d307c4f33580243
Vancha <tjipkevanderheide90@gmail.com>
2023-03-07 15:48:51 +0100

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,65 @@
+'''
+The array data structure. This file will hold a couple things related to working with arrays:
+- searching through an aray
+- reversing an array
+- rotating an array
+- array search,insert and delete in unsorted array
+- array search, insert and delete in sorted array
+- array sorting
+- generation of all subarrays
+
+After which the goal is to use that in solving a couple of the following questions:
+## Easy
+        Find the largest three elements in an array
+        Find Second largest element in an array
+        Move all zeroes to end of array
+        Rearrange array such that even positioned are greater than odd
+        Rearrange an array in maximum minimum form using Two Pointer Technique
+        Segregate even and odd numbers
+        Reversal algorithm for array rotation
+        Print left rotation of array in O(n) time and O(1) space
+        Sort an array in wave form
+        Sort an array which contain 1 to n values
+        Count the number of possible triangles
+        Print All Distinct Elements of a given integer array
+        Find the element that appears once in Array where every other element appears twice
+        Leaders in an array
+        Find sub-array with given sum
+## Medium
+        Rearrange an array such that arr[i] = i
+        Rearrange positive and negative numbers in O(n) time and O(1) extra space
+        Reorder an array according to given indexes
+        Search an element in a sorted and rotated array
+        Find the Rotation Count in Rotated Sorted array
+        K-th Largest Sum Contiguous Subarray
+        Find the smallest missing number
+        Difference Array | Range update query in O(1)
+        Maximum profit by buying and selling a share at most twice
+        Smallest subarray with sum greater than a given value
+        Inversion count in Array using Merge Sort
+        Sort an array of 0s, 1s and 2s
+        Merge two sorted arrays with O(1) extra space
+        Majority Element
+        Two Pointers Technique
+        Find a peak element
+        Find a triplet that sum to a given value
+        Minimum increment by k operations to make all elements equal
+        Equilibrium index of an array
+##  Hard
+        Find k numbers with most occurrences in the given array
+        MO’s Algorithm
+        Square Root (Sqrt) Decomposition Algorithm
+        Sparse Table
+        Range sum query using Sparse Table
+        Range Minimum Query (Square Root Decomposition and Sparse Table)
+        Range LCM Queries
+        Merge Sort Tree for Range Order Statistics
+        Minimum number of jumps to reach end
+        Space optimization using bit manipulations
+        Sort a nearly sorted (or K sorted) array
+        Find maximum value of Sum( i*arr[i]) with only rotations on given array allowed
+        Median in a stream of integers (running integers)
+        Construct an array from its pair-sum array
+        Maximum equlibrium sum in an array
+        Smallest Difference Triplet from Three arrays
+        Find all triplets with zero sum'''
