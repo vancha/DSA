31eb28cef33dc3e1ac0837a79d294bbded75ea97
Vancha <tjipkevanderheide90@gmail.com>
2023-03-10 11:18:42 +0100

    Update linkedlists.py

--- linked list/linkedlists.py
+++ linked list/linkedlists.py
@@ -1,8 +1,11 @@
 #singly linked list
-class singly_linked_list:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
+#this singly linked list is implemented like it is for a lot of the problems on leetcode. it consists of elements that have only one value and one pointer to the next element
+class ListNode:
+  def __init__(self, val=0, next=None):
+    #the elements value
+    self.val = val
+    #the pointer to the next element
+    self.next = next
     
 #doubly linked list
 #circular linked list
