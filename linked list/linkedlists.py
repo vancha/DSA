41161d99c03618de4ab20e5b9aca4c9cc835419d
Vancha <tjipkevanderheide90@gmail.com>
2023-03-10 11:27:18 +0100

    Update linkedlists.py

--- linked list/linkedlists.py
+++ linked list/linkedlists.py
@@ -1,19 +1,53 @@
-#singly linked list
-#this singly linked list is implemented like it is for a lot of the problems on leetcode. it consists of elements that have only one value and one pointer to the next element
+#SINGLY LINKED LIST
+#implementation:
 class ListNode:
   def __init__(self, val=0, next=None):
     #the elements value
     self.val = val
     #the pointer to the next element
     self.next = next
+
     
-#doubly linked list
-#this doubly linked list is implemented like it is for a lot of the problems on leetcode. it consists of elements that have only one value and two pointers: one to the next element in the list, and one to the previous element in the list
+#usage
+
+
+#END OF SINGLY LINKED LIST
+
+
+#DOUBLY LINKED LIST
+#implementation:
 class Node:
     def __init__(self, val, prev, next, child):
         self.val = val
         self.prev = prev
         self.next = next
-#circular linked list
-#circular singly linked list
-#circular doubly linked list
+
+        
+#usage
+
+
+#END OF DOUBLY LINKED LIST
+
+
+#CIRCULAR LINKED LIST
+#implementation
+
+#usage
+
+#END OF CIRCULAR LINKED LIST
+
+
+#CIRCULAR SINGLY LINKED LIST
+#implementation
+
+#usage
+
+#END OF CIRCULAR SINGLY LINKED LIST
+
+
+#CIRCULAR DOUBLY LINKED LIST
+#implementation
+
+#usage
+
+#END OF CIRCULAR DOUBLY LINKED LIST
