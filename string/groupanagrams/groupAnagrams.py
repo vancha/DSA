34ba85c562d495224d48028500eedcfdc8f8ddf0
Vancha <tjipkevanderheide90@gmail.com>
2023-04-14 14:31:38 +0200

    Update groupAnagrams.py

--- string/groupanagrams/groupAnagrams.py
+++ string/groupanagrams/groupAnagrams.py
@@ -1,1 +1,10 @@
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    word_list = {}
+    for word in strs:
+        srted = "".join(sorted(word)) 
+        if srted in word_list:
+            word_list[srted].append(word)
+        else:
+            word_list[srted] = [word]
 
+    return [word_list[key] for key in word_list]
