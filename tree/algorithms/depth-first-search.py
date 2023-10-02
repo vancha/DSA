a02c6d699d94a41758475053d1a37c91b2e8990e
Vancha <tjipkevanderheide90@gmail.com>
2023-10-02 10:59:39 +0200

    Update depth-first-search.py

--- tree/algorithms/depth-first-search.py
+++ tree/algorithms/depth-first-search.py
@@ -8,13 +8,13 @@ while there are elements on the stack:
     - mark the element as visited (it does not need to be checked if it is visited or not)
       - add it's children to the stack if any are present
 '''
-def depth_first_search(root, val):
+def depth_first_search(root, value):
     visited = {}
     stack = [root]
 
     while stack:
         element = stack.pop()
-        if element.val == val:
+        if element.val == value:
             return element
         visited[element] = True
         left = element.left
@@ -25,3 +25,17 @@ def depth_first_search(root, val):
             stack.append(right)
     
     return None
+
+'''
+usage of the depth first search algorightm:
+'''
+#creates a tree, with 1 as the root node, 2 as it's left node, and 3 as it's right node
+root = TreeNode(val=1, left=TreeNode(left=None,right=None,val= 2),right=TreeNode(left=None,right=None,val=3))
+
+#check if a certain value is in the search tree
+if depth_first_search(root, 9):
+    #print found if it is
+    print('found item')
+else:
+    #print not found if it is not
+    print('did not find item')
