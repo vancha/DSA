9aaadf24efa14c3359cce90e10a646ed270376d7
Vancha <tjipkevanderheide90@gmail.com>
2023-10-03 13:57:02 +0200

    Update trees.py

--- tree/trees.py
+++ tree/trees.py
@@ -1,5 +1,9 @@
+'''
+A simple binary tree data structure. All it exists of is a value, a possible left child, and a possible right child.
+since trees are recursive datastructures, those left and right children can in turn be binary trees themselves.
+'''
 class TreeNode:
      def __init__(self, val=0, left=None, right=None):
          self.val = val
-         self.left = left
-         self.right = right
+         self.left_child = left
+         self.right_child = right
