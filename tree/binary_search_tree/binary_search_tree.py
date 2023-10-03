f94f5bc27abc4031109fd7dde6a80e7b434c4579
Vancha <tjipkevanderheide90@gmail.com>
2023-10-03 14:05:11 +0200

    Update binary_search_tree.py

--- tree/binary_search_tree/binary_search_tree.py
+++ tree/binary_search_tree/binary_search_tree.py
@@ -7,21 +7,31 @@ performance improvement over regular binary trees
 '''
 class TreeNode:
     def __init__(self, value, left=None,right=None):
-        self.left  = left
-        self.right = right
+        self.left_child  = left
+        self.right_child = right
         self.value = value
-    
+        
+    #if this node has no left and right children, it's a leaf node
     def is_leaf_node(self):
-        return not self.right and not self.left
-
+        return not self.right_child and not self.left_child
+        
+    #insert takes the root of a truee, and a value to insert
     def insert(self, root, value):
-        if value < root.value:
-            if root.left:
-                insert(root.left, value)
+        #inside this function, root is known as "current_node", cause it's the node we are currently looking at
+        current_node = root
+
+        #if the value to insert is smaller than the current node's value
+        if value < current_node.value:
+            #and the current node has a left child
+            if current_node.left_child:
+                #then the new current node is set to be that left child, and we start from the top again
+                insert(current_node.left_child, value)
             else:
-                root.left = TreeNode(value)
+                #if not, the left child becomes a new node with the value to insert
+                current_node.left_child = TreeNode(value)
         else:
-            if root.right:
-                insert(root.right, value)
+            #we do the same for the right node as we've done for the left node above
+            if current_node.right_child:
+                insert(current_node.right_child, value)
             else:
-                root.right = TreeNode(value)
+                current_node.right_child = TreeNode(value)
